ed9173d3d4158d8a43140c82eb1bf3df23f0b22b
Fabian Grutsch <f.grutsch@gmail.com>
2021-11-14 00:18:16 +0000

    Update logback-classic to 1.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-persistence-tck"   % Versions.akka % Test,
     "org.mapdb"          % "mapdb"                  % "3.0.8",
     "org.scalatest"     %% "scalatest"              % "3.2.10"      % Test,
-    "ch.qos.logback"     % "logback-classic"        % "1.2.6"       % Test
+    "ch.qos.logback"     % "logback-classic"        % "1.2.7"       % Test
   )
 
   val betterMonadicFor: ModuleID = "com.olegpy"           %% "better-monadic-for" % "0.3.1"
