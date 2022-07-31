11e82c8665ba75f8f32c678f8961e0b490661518
Fabian Grutsch <f.grutsch@gmail.com>
2022-07-31 02:37:35 +0200

    Update scalatest to 3.2.13 (#74)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-persistence-tck"       % Versions.akka % Test,
     "com.typesafe.akka" %% "akka-serialization-jackson" % Versions.akka % Test,
     "org.mapdb"          % "mapdb"                      % "3.0.8",
-    "org.scalatest"     %% "scalatest"                  % "3.2.12"      % Test
+    "org.scalatest"     %% "scalatest"                  % "3.2.13"      % Test
   )
 
   val betterMonadicFor: ModuleID = "com.olegpy"           %% "better-monadic-for" % "0.3.1"
