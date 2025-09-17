ba5cdd28fafed747e63300443ef96c4552df2d41
Fabian Grutsch <f.grutsch@gmail.com>
2025-09-17 00:31:49 +0000

    Update file headers with sbt-header 5.11.0
    Executed command: sbt headerCreate

--- core/src/main/scala/com/fgrutsch/akka/persistence/mapdb/snapshot/MapDbSnapshotRepository.scala
+++ core/src/main/scala/com/fgrutsch/akka/persistence/mapdb/snapshot/MapDbSnapshotRepository.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 akka-persistence-mapdb contributors
+ * Copyright 2025 akka-persistence-mapdb contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
